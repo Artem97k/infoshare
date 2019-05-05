69258fbe35ce0c09fcaea3f3d6f3dafc45bbfa5e
Artyom Vetrov <artem97k@gmail.com>
2019-05-05 13:38:40 +0300

    Sessions controller added

---
+++ auth/config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'sessions/create'
+  get 'sessions/delete'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/create'
+  get 'users/update'
+  get 'users/delete'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
