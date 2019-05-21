d7f87e2274e104f3a92b034a71e0db98ea5972ee
Artyom Vetrov <artem97k@gmail.com>
2019-05-21 16:28:35 +0300

    Auth and gateway services names changed; scaffold of profile service created

---
+++ profile-service/db/migrate/20190521131919_create_profiles.rb
@@ -0,0 +1,14 @@
+class CreateProfiles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :profiles do |t|
+      t.string :user_id
+      t.string :name
+      t.string :surname
+      t.string :email
+      t.string :bio
+      t.string :avatar_id
+
+      t.timestamps
+    end
+  end
+end
