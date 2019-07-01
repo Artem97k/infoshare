b9a7db22ffcfa1b8af85a3def4784dc5fc8b5917
Artyom Vetrov <artem97k@gmail.com>
2019-07-01 18:55:28 +0300

    Switch operator added in search submit button function

--- gateway-service/public/search.js
+++ gateway-service/public/search.js
@@ -1,29 +1,32 @@
 $(document).on( "click", "#search_submit", function() {
   event.preventDefault();
   if ( $("#search_query").val() !== "") {
-    $.post({ url: "search",
-             data: { query: $("#search_query").val(),
-                     type: $("select").val() },
-             success: function (data) {
-                        if ( data[data.length-1].status == "Ok" ) {
-                          $("#search_display").remove();
-                          $("#info_display").text("");
-                          let page = set_search_result_page(data, search_result_page);
-                          $("#main").append(page);
-                        } else { 
-                          $("#search_display").remove();
-                          $("#info_display").attr("style", "color: red;");
-                          $("#info_display").text("Nothing found!");
+    switch ($("select").val()) {
+      case 'profile':
+        $.post({ url: "search",
+                 data: { query: $("#search_query").val(),
+                         type: $("select").val() },
+                 success: function (data) {
+                            if ( data[data.length-1].status == "Ok" ) {
+                              $("#search_display").remove();
+                              $("#info_display").text("");
+                              let page = set_profile_search_result_page(data, search_result_page);
+                              $("#main").append(page);
+                            } else { 
+                              $("#search_display").remove();
+                              $("#info_display").attr("style", "color: red;");
+                              $("#info_display").text("Nothing found!");
+                            }
+                          },
+                 error: function (data) {
+                          alert("Server error!");
                         }
-                      },
-             error: function (data) {
-                      alert("Server error!");
-                    }
-    });
+        });
+    }
   }
 });
 
-function set_search_result_page(search_data, page_form) {
+function set_profile_search_result_page(search_data, page_form) {
   search_data.splice(-1,1);
   let page = $(page_form);
   let row = '<tr><th>Profile link</th><th>Name</th><th>Surname</th></tr>';
