18f961d0f3dc73251312f3280651b6237e2c801d
Artem Vetrov <artem97k@gmail.com>
2019-07-09 15:24:34 +0300

    DB migration command added to script

--- runme.sh
+++ runme.sh
@@ -6,4 +6,6 @@ bundle install
 
 cd ..
 
+(cd gateway-service; rake db:migrate) & (cd auth-service; rake db:migrate) & (cd profile-service; rake db:migrate) & (cd article-service; rake db:migrate) & (cd series-service; rake db:migrate)
+
 (cd gateway-service; rails s -p 3000) & (cd auth-service; rails s -p 3001) & (cd profile-service; rails s -p 3002) & (cd article-service; rails s -p 3003) & (cd series-service; rails s -p 3004)
